c1f5694cc7ae2117c5c9f5fbc330a0f223e00e35
Tanmay Malhotra <malhotratanmay04@gmail.com>
2024-06-30 23:13:48 +0530

    further changes

---
+++ applications/route.py
@@ -0,0 +1,15 @@
+from flask import render_template, request
+from flask import current_app as app
+
+@app.route('/')
+def home():
+    return render_template('homepage.html')
+
+#logic for user registration
+@app.route('/signup', methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        name = request.form.get('name')
+        email=request.form.get('email')
+        password = request.form.get('password')
